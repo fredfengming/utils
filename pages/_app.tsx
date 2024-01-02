7de2c2701372cad2117e23ab47d969ba84c57961
Ming Feng <fredfengming@hotmail.com>
2024-01-02 15:08:22 +0000

    test GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import { AppProps } from "next/app";
+import Script from "next/script";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-MTN2Z2J7QN`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-MTN2Z2J7QN', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
